9b463eebc0646e5b752c2015db0cf6c70d9d2d7f
Precious Ayomide Fatoba~ <pfatoba8@gmail.com>
2022-09-22 16:19:50 -0700

    precious

---
+++ perfume.js
@@ -0,0 +1,22 @@
+//alert("Hello World!","i am here");
+//let message="lets get started";
+//let hello=message;
+//alert(message,hello);
+//let name="john";
+//let admin=name;
+//alert(admin);
+/*let my_planet="Earth";
+let user="precious";
+alert(my_planet);
+alert(user);
+const birthday="01-06-2003";
+const age=someCode(birthday);
+alert(age);*/
+/*let a = 1, b = 1;
+
+alert( ++a ); // 2, prefix form returns the new value
+alert( b++ ); // 1, postfix form returns the old value
+
+alert( a ); // 2, incremented once*/
+let result = (a + b < 4) ? 'Below' : 'Over';
+
